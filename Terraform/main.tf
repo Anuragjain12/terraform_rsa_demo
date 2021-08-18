0b504cf4e06a6dbc35008a633212f5f68848d900
Anuragjain12 <jainanurag470@gmail.com>
2021-08-18 15:10:21 +0100

    chnages

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,16 +1,11 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>2.31.1"
-    }
-  }
+  backend "azure" {}
 }
 
 provider "azurerm" {
+  version = "=2.20.0"
   features {}
-  subscription_id = "edafcb5d-a934-4403-b146-138315cc7e47"
-}
+  }
 
 resource "azurerm_resource_group" "demo_rg" {
   name     = "example"
@@ -19,8 +14,4 @@ resource "azurerm_resource_group" "demo_rg" {
     env    = "demo"
     source = "terraform"
   }
-}
-
-output "id" {
-value = data.azurerm_resource_group.demo_rg.id
 }
